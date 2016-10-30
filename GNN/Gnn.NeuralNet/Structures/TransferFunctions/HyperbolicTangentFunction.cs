232fe4d0e850236905508f55e16cb271f7aa202c
GWigWam <welwisam@gmail.com>
2016-10-30 21:07:08 +0100

    Add transfer function
    Hyperbolic-tangent & sigmoid. Includes two images of the functions'
    graphs

---
+++ GNN/Gnn.NeuralNet/Structures/TransferFunctions/HyperbolicTangentFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnn.NeuralNet.Structures.TransferFunctions {
+
+    public class HyperbolicTangentFunction : TransferFunction {
+        public override float XMax => 2;
+        public override float XMin => -2;
+        public override float YMax => 1;
+        public override float YMin => -1;
+
+        public override float Calculate(float input) {
+            var val = Math.Tanh(input);
+            return (float)val;
+        }
+    }
+}
---
+++ GNN/Gnn.NeuralNet/Structures/TransferFunctions/SigmoidFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnn.NeuralNet.Structures.TransferFunctions {
+
+    public class SigmoidFunction : TransferFunction {
+        public override float XMax => 4;
+        public override float XMin => -4;
+        public override float YMax => 1;
+        public override float YMin => 0;
+
+        public override float Calculate(float input) {
+            var val = (1.0 / (1.0 + Math.Pow(Math.E, -input)));
+            return (float)val;
+        }
+    }
+}
