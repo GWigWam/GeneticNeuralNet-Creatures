b6e0c2a5b805408fa35a56785a3f4b758c1bd38f
GWigWam <welwisam@gmail.com>
2016-11-06 16:37:23 +0100

    Add crossover code
    Using uniform crossover, where every float in the 'chromosone' array has
    a 50% chance of being swapped

---
+++ GNN/Gnn.Genetic/Procedures/Crossover.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnn.Genetic.Procedures {
+
+    public static class Crossover {
+        private static Random random;
+
+        static Crossover() {
+            random = new Random();
+        }
+
+        public static IEnumerable<Individual> UniformCrossoverPop(IEnumerable<Individual> source) {
+            var shuffled = source.OrderBy(i => random.NextDouble()).ToArray();
+            for(int i = 1; i < shuffled.Length; i += 2) {
+                var cRes = UniformCrossover(shuffled[i - 1], shuffled[i]);
+                yield return cRes.Item1;
+                yield return cRes.Item2;
+            }
+        }
+
+        public static Tuple<Individual, Individual> UniformCrossover(Individual in0, Individual in1) {
+            var length = in0.Values.Length;
+            var r0 = new float[length];
+            var r1 = new float[length];
+
+            for(int i = 0; i < length; i++) {
+                var cross = random.NextDouble() < 0.5;
+                r0[i] = cross ? in1.Values[i] : in0.Values[i];
+                r1[i] = cross ? in0.Values[i] : in1.Values[i];
+            }
+
+            return Tuple.Create(new Individual(r0), new Individual(r1));
+        }
+    }
+}
