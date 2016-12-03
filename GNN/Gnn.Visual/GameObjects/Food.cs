48d7da93ccfe13da2a7251f919012ed10fd6988a
GWigWam <welwisam@gmail.com>
2016-12-03 14:24:09 +0100

    Set const for max food health

--- GNN/Gnn.Visual/GameObjects/Food.cs
+++ GNN/Gnn.Visual/GameObjects/Food.cs
@@ -10,16 +10,17 @@ using System.Threading.Tasks;
 namespace Gnn.Visual.GameObjects {
 
     public class Food : GameObject, IWorldVisible {
-        private const float AddHealthPerSecond = 1f / 25f;
+        private const float MaxHealth = 0.7f;
+        private const float AddHealthPerSecond = MaxHealth / 10f;
 
         private float health;
 
         public float Health {
             get { return health; }
-            set { health = value > 1 ? 1 : value; }
+            set { health = value > MaxHealth ? MaxHealth : value; }
         }
 
-        public Vector2 Color => new Vector2(0, health);
+        public Vector2 Color => new Vector2(0, health / MaxHealth);
 
         public Food(World world, MainGameContent res, Vector2 position) : base(world, res.TFood, position) {
         }
@@ -30,12 +31,12 @@ namespace Gnn.Visual.GameObjects {
         }
 
         public override void Move(float secsPassed) {
-            Rotation += (0.01f * (float)Math.Pow(health * 100, 1.3)) * secsPassed;
+            Rotation += (0.01f * (float)Math.Pow((health / MaxHealth) * 100, 1.3)) * secsPassed;
         }
 
         public override void Draw(SpriteBatch sb) {
             var rg = Color;
-            var col = new Color(rg.X, rg.Y, 0, Health + 0.3f);
+            var col = new Color(rg.X, rg.Y, 0, (Health / MaxHealth) + 0.3f);
             sb.Draw(Texture, new Rectangle(DrawX, DrawY, Texture.Width, Texture.Height), null, col, Rotation, Origin, SpriteEffects.None, 0);
         }
 
