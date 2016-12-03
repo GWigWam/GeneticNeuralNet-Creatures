7c5ef4eed2c143dfb7cb8a49c89c8f67a59d250c
GWigWam <welwisam@gmail.com>
2016-12-03 21:44:09 +0100

    Stat performance improvement

--- GNN/Gnn.Genetic/StatHelper.cs
+++ GNN/Gnn.Genetic/StatHelper.cs
@@ -10,9 +10,12 @@ namespace Gnn.Genetic {
     public static class StatHelper {
 
         public static float[] VarietyPerGene(this IEnumerable<Individual> source) {
+            float[][] vals = source.Select(i => i.Values).ToArray();
+
             var res = new float[source.First().Values.Length];
             for(int i = 0; i < source.First().Values.Length; i++) {
-                res[i] = MathHelper.StdDeviation(source.Select(ind => ind.Values[i]), Grouptype.Population);
+                //res[i] = MathHelper.StdDeviation(source.Select(ind => ind.Values[i]), Grouptype.Population);
+                res[i] = MathHelper.StdDeviation(vals.Select(a => a[i]).ToArray(), Grouptype.Population);
             }
             return res;
         }
